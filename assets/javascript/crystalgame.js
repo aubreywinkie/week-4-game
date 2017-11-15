06f286909112f94f3c7a51923a9447457de40f6e
Aubrey Winkie <aubreywinkie@utexas.edu>
2017-11-15 11:56:40 -0600

    working crystal game

--- assets/javascript/crystalgame.js
+++ assets/javascript/crystalgame.js
@@ -1,35 +1,35 @@
 var targetNumber = [Math.floor(Math.random() * 102 + 19 )];
-// not sure if this is how to get a random value between 19-120
+
 
 $("#number-to-guess").text(targetNumber);
 var counter = 0;
-var numberGen = [Math.floor((Math.random() * 12 ) + 1)];
-// var options2 = [Math.floor((Math.random() * 12 ) + 1)];
-// var options3 = [Math.floor((Math.random() * 12 ) + 1)];
-// var options4 = [Math.floor((Math.random() * 12 ) + 1)];
+var numGen1 = [Math.floor((Math.random() * 12 ) + 1)];
+var numGen2 = [Math.floor((Math.random() * 12 ) + 1)];
+var numGen3 = [Math.floor((Math.random() * 12 ) + 1)];
+var numGen4 = [Math.floor((Math.random() * 12 ) + 1)];
+var win = 0
+var loss = 0
 
 
 var numberOptions = [
 {
-	option: numberGen,
+	option: numGen1,
 	img: src="https://vignette.wikia.nocookie.net/fantendo/images/3/31/Crystal.png/revision/latest?cb=20150419210156" }, 
 {
-	option: numberGen,
+	option: numGen2,
 	img: src="https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/c/c3/Crystal_iso8.png/revision/latest?cb=20151121235324"}, 
 {
-	option: numberGen,
+	option: numGen3,
 	img: src="https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/b/b4/Crystal_quest.png/revision/latest?cb=20151122000045"}, 
 {
-	option: numberGen,
+	option: numGen4,
 	img: src="https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/b/b4/Crystal_quest.png/revision/latest?cb=20151122000045"}
 ];
-// is this how you get 4 separate random numbers between 1- 12?
 
 for (var i = 0; i < numberOptions.length; i++) {
 	var imageCrystal = $("<img>");
 	imageCrystal.addClass("crystal-image");
 	imageCrystal.attr('src', numberOptions[i].img);
-	// need to figure out how to have 4 different crystal images
 	imageCrystal.attr("data-crystalvalue", numberOptions[i].option);
 	$("#crystals").append(imageCrystal);
 }
@@ -40,25 +40,39 @@ $(".crystal-image").on("click", function() {
 	crystalValue = parseInt(crystalValue);
 	counter += crystalValue;
 	alert("New score: " + counter);
-
-    if (counter === targetNumber) {
-      alert("You win!");
-    }
-
-    else if (counter >= targetNumber) {
-      alert("You lose!!");
-    }
-
+	checkWin();
+	checkLoss();
+	
+	
 });
 
-// function checkWin() {
-// 	if counter === numberGen {
-// 		alert('YOU WIN!!!');
-// 		reset();
-// 	}
-// }
+function checkWin() {
+	if(counter == targetNumber) {
+		alert('YOU WIN!!!');
+		reset();
+	} else {
+		return;
+	}
+	win += 1;
+	$("#win").text(win);
+}
+function checkLoss() {
+	if(counter > targetNumber){
+		alert("YOU LOSE");
+		reset();
+	} else {
+		return;
+	}
+	loss +=1;
+	$("#loss").text(loss);
+}
+    
+function reset() {
+	counter = 0;
+	numGen1 = [Math.floor((Math.random() * 12 ) + 1)];
+	numGen2 = [Math.floor((Math.random() * 12 ) + 1)];
+	numGen3 = [Math.floor((Math.random() * 12 ) + 1)];
+	numGen4 = [Math.floor((Math.random() * 12 ) + 1)];
+}
 
-// function checkLoss {
-
-// }
 
