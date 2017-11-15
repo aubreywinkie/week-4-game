53bcdaf554aa4d17acc4925e5231432f6c9664ac
Aubrey Winkie <aubreywinkie@utexas.edu>
2017-11-14 18:42:16 -0600

    crystal addition working

--- assets/javascript/crystalgame.js
+++ assets/javascript/crystalgame.js
@@ -3,21 +3,34 @@ var targetNumber = [Math.floor(Math.random() * 102 + 19 )];
 
 $("#number-to-guess").text(targetNumber);
 var counter = 0;
-var options1 = [Math.floor((Math.random() * 12 ) + 1)];
-var options2 = [Math.floor((Math.random() * 12 ) + 1)];
-var options3 = [Math.floor((Math.random() * 12 ) + 1)];
-var options4 = [Math.floor((Math.random() * 12 ) + 1)];
+var numberGen = [Math.floor((Math.random() * 12 ) + 1)];
+// var options2 = [Math.floor((Math.random() * 12 ) + 1)];
+// var options3 = [Math.floor((Math.random() * 12 ) + 1)];
+// var options4 = [Math.floor((Math.random() * 12 ) + 1)];
 
-var numberOptions = [options1, options2, options3, options4];
+
+var numberOptions = [
+{
+	option: numberGen,
+	img: src="https://vignette.wikia.nocookie.net/fantendo/images/3/31/Crystal.png/revision/latest?cb=20150419210156" }, 
+{
+	option: numberGen,
+	img: src="https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/c/c3/Crystal_iso8.png/revision/latest?cb=20151121235324"}, 
+{
+	option: numberGen,
+	img: src="https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/b/b4/Crystal_quest.png/revision/latest?cb=20151122000045"}, 
+{
+	option: numberGen,
+	img: src="https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/b/b4/Crystal_quest.png/revision/latest?cb=20151122000045"}
+];
 // is this how you get 4 separate random numbers between 1- 12?
 
 for (var i = 0; i < numberOptions.length; i++) {
 	var imageCrystal = $("<img>");
 	imageCrystal.addClass("crystal-image");
-	imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-	// image wont show up
+	imageCrystal.attr('src', numberOptions[i].img);
 	// need to figure out how to have 4 different crystal images
-	imageCrystal.atrr("data-crystalvalue", numberOptions[i]);
+	imageCrystal.attr("data-crystalvalue", numberOptions[i].option);
 	$("#crystals").append(imageCrystal);
 }
 
@@ -37,4 +50,15 @@ $(".crystal-image").on("click", function() {
     }
 
 });
+
+// function checkWin() {
+// 	if counter === numberGen {
+// 		alert('YOU WIN!!!');
+// 		reset();
+// 	}
+// }
+
+// function checkLoss {
+
+// }
 
